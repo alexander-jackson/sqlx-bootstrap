066212f46ff7fb8e53efd85d50cd5a9a4efe2d02
Alexander Jackson <alexanderjackson@protonmail.com>
2022-05-07 14:56:25 +0100

    refactor: split config into multiple structs

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -3,13 +3,14 @@ use sqlx_bootstrap::BootstrapConfig;
 #[tokio::main]
 async fn main() -> sqlx::Result<()> {
     let config = BootstrapConfig::new(
-        String::from("root"),
-        String::from("password"),
-        String::from("postgres"),
-        String::from("serviceblue"),
-        String::from("someotherpassword"),
-        String::from("service"),
-        String::from("localhost:5432"),
+        "root",
+        "password",
+        "postgres",
+        "serviceblue",
+        "someotherpassword",
+        "service",
+        "localhost",
+        5432,
     );
 
     config.bootstrap().await?;
--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,83 @@
-use sqlx::{Connection, PgConnection};
+use sqlx::{postgres::PgConnectOptions, Connection, PgConnection};
 
 #[derive(Clone, Debug)]
-pub struct BootstrapConfig {
-    root_username: String,
-    root_password: String,
-    root_database_name: String,
-    database_username: String,
-    database_password: String,
-    database_name: String,
-    database_uri: String,
+pub struct RootConfig<'a> {
+    username: &'a str,
+    password: &'a str,
+    database: &'a str,
 }
 
-impl BootstrapConfig {
+impl<'a> RootConfig<'a> {
+    pub fn new(username: &'a str, password: &'a str, database: &'a str) -> Self {
+        Self {
+            username,
+            password,
+            database,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ApplicationConfig<'a> {
+    username: &'a str,
+    password: &'a str,
+    database: &'a str,
+}
+
+impl<'a> ApplicationConfig<'a> {
+    pub fn new(username: &'a str, password: &'a str, database: &'a str) -> Self {
+        Self {
+            username,
+            password,
+            database,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ConnectionConfig<'a> {
+    host: &'a str,
+    port: u16,
+}
+
+impl<'a> ConnectionConfig<'a> {
+    pub fn new(host: &'a str, port: u16) -> Self {
+        Self { host, port }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct BootstrapConfig<'a> {
+    root: RootConfig<'a>,
+    app: ApplicationConfig<'a>,
+    conn: ConnectionConfig<'a>,
+}
+
+impl<'a> BootstrapConfig<'a> {
     pub fn new(
-        root_username: String,
-        root_password: String,
-        root_database_name: String,
-        database_username: String,
-        database_password: String,
-        database_name: String,
-        database_uri: String,
+        root_username: &'a str,
+        root_password: &'a str,
+        root_database_name: &'a str,
+        database_username: &'a str,
+        database_password: &'a str,
+        database_name: &'a str,
+        host: &'a str,
+        port: u16,
     ) -> Self {
         Self {
-            root_username,
-            root_password,
-            root_database_name,
-            database_username,
-            database_password,
-            database_name,
-            database_uri,
+            root: RootConfig::new(root_username, root_password, root_database_name),
+            app: ApplicationConfig::new(database_username, database_password, database_name),
+            conn: ConnectionConfig::new(host, port),
         }
     }
 
-    async fn bootstrap_user(&self) -> sqlx::Result<()> {
-        let url = format!(
-            "postgres://{}:{}@{}/{}",
-            self.root_username, self.root_password, self.database_uri, self.root_database_name,
-        );
-
-        let mut conn = PgConnection::connect(&url).await?;
-
+    async fn bootstrap_user(&self, conn: &mut PgConnection) -> sqlx::Result<()> {
         // Check whether the role already exists
         let existing_role = sqlx::query!(
             "SELECT oid FROM pg_roles WHERE rolname = $1",
-            self.database_username
+            self.app.username
         )
-        .fetch_optional(&mut conn)
+        .fetch_optional(&mut *conn)
         .await?;
 
         if existing_role.is_some() {
@@ -56,16 +88,16 @@ impl BootstrapConfig {
         // `sqlx` doesn't seem to like doing this in `query!`
         let query = format!(
             "CREATE USER {} PASSWORD '{}'",
-            self.database_username, self.database_password
+            self.app.username, self.app.password
         );
 
-        sqlx::query(&query).execute(&mut conn).await?;
+        sqlx::query(&query).execute(conn).await?;
 
         Ok(())
     }
 
     async fn grant_role_to_root(&self, conn: &mut PgConnection) -> sqlx::Result<()> {
-        let query = format!("GRANT {} TO {}", self.database_username, self.root_username);
+        let query = format!("GRANT {} TO {}", self.app.username, self.root.username);
 
         sqlx::query(&query).execute(conn).await?;
 
@@ -75,7 +107,7 @@ impl BootstrapConfig {
     async fn create_database(&self, conn: &mut PgConnection) -> sqlx::Result<()> {
         let query = format!(
             "CREATE DATABASE {} OWNER {}",
-            self.database_name, self.database_username
+            self.app.database, self.app.username
         );
 
         sqlx::query(&query).execute(conn).await?;
@@ -84,30 +116,20 @@ impl BootstrapConfig {
     }
 
     async fn revoke_role_from_root(&self, conn: &mut PgConnection) -> sqlx::Result<()> {
-        let query = format!(
-            "REVOKE {} FROM {}",
-            self.database_username, self.root_username
-        );
+        let query = format!("REVOKE {} FROM {}", self.app.username, self.root.username);
 
         sqlx::query(&query).execute(conn).await?;
 
         Ok(())
     }
 
-    async fn bootstrap_database(&self) -> sqlx::Result<()> {
-        let url = format!(
-            "postgres://{}:{}@{}/{}",
-            self.root_username, self.root_password, self.database_uri, self.root_database_name,
-        );
-
-        let mut conn = PgConnection::connect(&url).await?;
-
+    async fn bootstrap_database(&self, conn: &mut PgConnection) -> sqlx::Result<()> {
         // Check whether the database already exists
         let existing_database = sqlx::query!(
             "SELECT oid FROM pg_database WHERE datname = $1",
-            self.database_name
+            self.app.database
         )
-        .fetch_optional(&mut conn)
+        .fetch_optional(&mut *conn)
         .await?;
 
         if existing_database.is_some() {
@@ -115,18 +137,27 @@ impl BootstrapConfig {
             return Ok(());
         }
 
-        self.grant_role_to_root(&mut conn).await?;
-        self.create_database(&mut conn).await?;
-        self.revoke_role_from_root(&mut conn).await?;
+        self.grant_role_to_root(conn).await?;
+        self.create_database(conn).await?;
+        self.revoke_role_from_root(conn).await?;
 
         Ok(())
     }
 
     pub async fn bootstrap(&self) -> sqlx::Result<()> {
-        println!("Bootstrapping the database! Config: {:?}", self);
+        let options = PgConnectOptions::new()
+            .host(&self.conn.host)
+            .port(self.conn.port)
+            .username(&self.root.username)
+            .password(&self.root.password)
+            .database(&self.root.database);
 
-        self.bootstrap_user().await?;
-        self.bootstrap_database().await?;
+        dbg!(&options);
+
+        let mut conn = PgConnection::connect_with(&options).await?;
+
+        self.bootstrap_user(&mut conn).await?;
+        self.bootstrap_database(&mut conn).await?;
 
         Ok(())
     }
